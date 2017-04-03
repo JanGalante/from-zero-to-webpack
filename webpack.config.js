ab10728db4ed13b914fa2d6180e41b517ad6a311
Jan Lundholm Galante <jan.lundholm.galante@thomascook.se>
2017-04-03 11:23:52 +0200

    Modernizing the server Making new entry in webpack for server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
+const clientConfig = {
     entry: path.resolve('./src/index.browser.js'),
     output: {
         path: path.resolve('./dist'),
@@ -16,4 +17,31 @@ module.exports = {
             }
         ],
     }
-}
+};
+
+const serverConfig = {
+    target: 'node',
+
+    node: {
+        __dirname: true //true: sets _dirname to what it was in the source file. ./src/ in our case.
+    },
+
+    entry: path.resolve('./src/index.server.js'),
+    output: {
+        path: path.resolve('./dist'),
+        filename: 'server.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve('./src'),
+                loader: 'babel-loader',
+                query: require('./babelrc.js'), //this is added since we changed the filename
+            }
+        ],
+    }
+};
+
+// Notice that both configurations are exported
+module.exports = [clientConfig, serverConfig];
