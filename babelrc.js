44c02ead4025830589d856a78a4b413270cf1639
Jan Lundholm Galante <jan.lundholm.galante@thomascook.se>
2017-04-03 13:51:09 +0200

    Utilizing “tree shaking” in webpack

--- babelrc.js
+++ babelrc.js
@@ -3,7 +3,8 @@ module.exports = ({ server } = {} ) => ({
     ['env', {
       // targets: { node: 'current' } //target a node version
       // targets: { browsers: ['> 5%', 'last 2 versions'] } //target the browser
-      targets: server ? { node: 'current' } : { browsers: ['> 5%', 'last 2 versions'] }
+      targets: server ? { node: 'current' } : { browsers: ['> 5%', 'last 2 versions'] },
+      modules: false // To enables Webpacks "tree shaking" we just need to tell Babel not to transform javascript modules
     }],
 
     'react',
