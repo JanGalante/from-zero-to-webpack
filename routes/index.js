d5c999eaf7314e6fdbbc98ed437ff31f7d6e4823
Jan Lundholm Galante <jan.lundholm.galante@thomascook.se>
2017-04-04 12:11:26 +0200

    Adding Pug layout view and using Bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 //GET about page
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Hey my man' })
+  res.render('index', { title: 'The Pug section' })
 });
 
 //GET home page
 router.get('/about', function (req, res, next) {
-  res.render('about', { title: 'Hey', message: 'Hello there!' })
+  res.render('about', { title: 'About', message: 'This is the Pug about page' })
 });
 
 //GET user listing
