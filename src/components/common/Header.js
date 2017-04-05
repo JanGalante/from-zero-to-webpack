327e6b0710c1c7a1d67e92f801d6fcde2cf91db3
Jan Lundholm Galante <jan.lundholm.galante@thomascook.se>
2017-04-05 14:13:31 +0200

    Creating Course page

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -6,6 +6,8 @@ const Header = () => {
         <nav>
             <IndexLink to="/" activeClassName="active">Home</IndexLink>
             { " | " }
+            <IndexLink to="/courses" activeClassName="active">Courses</IndexLink>
+            { " | " }
             <IndexLink to="/about" activeClassName="active">About</IndexLink>
         </nav>
     );
--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,14 @@ import { Route, IndexRoute } from 'react-router';
 import Shell from './components/Shell';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
-// import CoursesPage from './components/course/CoursesPage';
+import CoursesPage from './components/course/CoursesPage';
 // import ManageCoursePage from './components/course/ManageCoursePage'; //eslint-disable-line import/no-named-as-default
 
 export default (
   <Route path="/" component={Shell}>
     <IndexRoute component={HomePage} />
-    {/*<Route path="courses" component={CoursesPage} />
-    <Route path="course" component={ManageCoursePage} />
+    <Route path="courses" component={CoursesPage} />
+    {/*<Route path="course" component={ManageCoursePage} />
     <Route path="course/:id" component={ManageCoursePage} />*/}
     <Route path="about" component={AboutPage} />
   </Route>
